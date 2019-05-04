0e164ff02b110340747e0b464c0b8e9aa7fdc4ec
adjohnston <adam.johnston3679@gmail.com>
2019-05-04 16:45:19 +0100

    rename the prop for click events

--- packages/components/src/Card/Card.tsx
+++ packages/components/src/Card/Card.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Container, Inner } from './Card.styles'
 
 export interface Props {
-  clickHandler: React.EventHandler<React.SyntheticEvent<HTMLButtonElement>>
+  onClick: React.EventHandler<React.SyntheticEvent<HTMLButtonElement>>
   children: React.ReactChildren
 }
 
-export const Card = React.memo(({ clickHandler, children }: Props) => {
+export const Card = React.memo(({ onClick, children }: Props) => {
   return (
-    <Container onClick={clickHandler}>
+    <Container onClick={onClick}>
       <Inner>{children}</Inner>
     </Container>
   )
