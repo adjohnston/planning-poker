bd42fb49e2fe437f3f6fd9363e251721bca60b87
adjohnston <adam.johnston3679@gmail.com>
2019-05-06 13:05:14 +0100

    add className prop so it can be re-styled

--- packages/components/src/List/List.tsx
+++ packages/components/src/List/List.tsx
@@ -3,6 +3,7 @@ import { List as StyledList, Item } from './List.styles'
 
 export interface Props {
   items: React.ReactNode[]
+  className: string
 }
 
 const renderItem = (node: React.ReactNode, index: number) => {
@@ -10,7 +11,9 @@ const renderItem = (node: React.ReactNode, index: number) => {
 }
 
 export const List = memo(
-  ({ items }: Props): React.ReactElement => {
-    return <StyledList>{items.map(renderItem)}</StyledList>
+  ({ items, className }: Props): React.ReactElement => {
+    return (
+      <StyledList className={className}>{items.map(renderItem)}</StyledList>
+    )
   },
 )
