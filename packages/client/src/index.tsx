b7e37e3c98bc28f7e3aef762508b2a6456d3b83b
adjohnston <adam.johnston3679@gmail.com>
2019-05-06 13:04:11 +0100

    add styled components to client

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,7 +4,6 @@ import App from './components/containers/App/App'
 import { StateProvider } from './components/utils/WithState/WithState'
 import { ActionsProvider } from './components/utils/WithActions/WithActions'
 
-import '@planning-poker/components/dist/components.css'
 import './styles/index.css'
 
 render(
